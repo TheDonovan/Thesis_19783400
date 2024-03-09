c35fcbc10c4d5ce16f07c887a1a2349019a84da0
BuildTools <unconfigured@null.spigotmc.org>
2024-03-09 15:47:04 +0800

    Finished Blank Slate

--- src/main.c
+++ src/main.c
@@ -4,13 +4,15 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr.h>
+#include <zephyr/kernel.h>
 
-#include <logging/log.h>
-LOG_MODULE_REGISTER(app_main);
+//#include <logging/log.h>
 
-void main(void)
+//LOG_MODULE_REGISTER(app_main);
+
+int main(void)
 {
-	LOG_INF("Hello World! %s", CONFIG_BOARD);
-	LOG_INF("Version: %s", CONFIG_APP_VERSION);
+	//LOG_INF("Hello World! %s", CONFIG_BOARD);
+	//LOG_INF("Version: %s", CONFIG_APP_VERSION);
+	return(0);
 }
