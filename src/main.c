372d122c2eeb813d712bde32c236374c7f4d8777
BuildTools <unconfigured@null.spigotmc.org>
2024-03-10 11:32:06 +0800

    added config settings to samv71

--- src/main.c
+++ src/main.c
@@ -6,13 +6,11 @@
 
 #include <zephyr/kernel.h>
 
-//#include <logging/log.h>
-
-//LOG_MODULE_REGISTER(app_main);
-
 int main(void)
 {
-	//LOG_INF("Hello World! %s", CONFIG_BOARD);
-	//LOG_INF("Version: %s", CONFIG_APP_VERSION);
+
+	//VARIABLES
+
+	
 	return(0);
 }
