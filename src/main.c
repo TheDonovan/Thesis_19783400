7998a951b517d4a1543c0ceac7b30aa77a5e20a9
BuildTools <unconfigured@null.spigotmc.org>
2024-03-15 13:02:02 +0800

    testing, no change

--- src/main.c
+++ src/main.c
@@ -14,20 +14,5 @@
 
 int main(void)
 {
-/*
-	//VARIABLES
-	uint8_t my_buffer[4] = {0};
-	struct spi_buf spi_buffer[1];
-	spi_buffer[0].buf = my_buffer;
-	spi_buffer[0].len = 4;
-	const struct spi_buf_set rx_buff = { spi_buffer, 1};
 
-	const struct spi_dt_spec lan9370_dev =
-                SPI_DT_SPEC_GET(DT_NODELABEL(lan9370), SPI_OP, 0);
-
-	ret = spi_read_dt(&lan9370_dev, &rx_buff);
-	if (ret) { LOG_INF("spi_read status: %d", ret); }
-
-	return(0);
-*/
 }
