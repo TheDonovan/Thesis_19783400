f7d284fc430074b54bb18ec0daf30e79a47b6b74
BuildTools <unconfigured@null.spigotmc.org>
2024-03-22 15:29:19 +0800

    Small revisions

--- src/main.c
+++ src/main.c
@@ -5,6 +5,9 @@
  */
 
 //INCLUDES
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(log_register, LOG_LEVEL_DBG);
+
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/spi.h>
@@ -12,7 +15,19 @@
 //DEFINES
 //#define SPI_OP  SPI_OP_MODE_MASTER |SPI_MODE_CPOL | SPI_MODE_CPHA | SPI_WORD_SET(8) | SPI_LINES_SINGLE
 
+#if IS_ENABLED(CONFIG_NET_L2_ETHERNET)
+#include <zephyr/net/net_if.h>
+#endif
+
 int main(void)
 {
+    //BASED OFF: https://github.com/golioth/samples/tree/main/hello-ethernet
+    if(IS_ENABLED(CONFIG_NET_L2_ETHERNET))
+    {
+        LOG_INF("Connected to Ethernet");
+        struct net_if *iface;
+        iface = net_if_get_default();
+        net_dhcpv4_start(iface);
+    }    
 
 }
