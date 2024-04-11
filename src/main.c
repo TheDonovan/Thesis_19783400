07dc856256027bb6854e923c346bacd64d1126af
BuildTools <unconfigured@null.spigotmc.org>
2024-04-11 11:44:52 +0800

    Correctly Implimented Overlay FIle For LED3

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
  */
 
 //INCLUDES
+#include <stdio.h>
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(log_register, LOG_LEVEL_DBG);
 
@@ -14,6 +15,10 @@ LOG_MODULE_REGISTER(log_register, LOG_LEVEL_DBG);
 
 //DEFINES
 //#define SPI_OP  SPI_OP_MODE_MASTER |SPI_MODE_CPOL | SPI_MODE_CPHA | SPI_WORD_SET(8) | SPI_LINES_SINGLE
+#define SLEEP_TIME_MS 1000
+#define LED3_NODE DT_ALIAS(led3)
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED3_NODE, gpios);
 
 #if IS_ENABLED(CONFIG_NET_L2_ETHERNET)
 #include <zephyr/net/net_if.h>
@@ -21,6 +26,7 @@ LOG_MODULE_REGISTER(log_register, LOG_LEVEL_DBG);
 
 int main(void)
 {
+
     //BASED OFF: https://github.com/golioth/samples/tree/main/hello-ethernet
     if(IS_ENABLED(CONFIG_NET_L2_ETHERNET))
     {
@@ -28,6 +34,32 @@ int main(void)
         struct net_if *iface;
         iface = net_if_get_default();
         net_dhcpv4_start(iface);
-    }    
+    }
+
+
+    int ret;
+
+
+    if(!gpio_is_ready_dt(&led))
+    {
+        return;
+    }
+
+    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+    
+    if(ret < 0)
+    {
+        return;
+    }
+
+    while(1)
+    {
+        ret = gpio_pin_toggle_dt(&led);
+        if(ret < 0)
+        {
+            return;
+        }
+        k_msleep(SLEEP_TIME_MS);
+    }
 
 }
